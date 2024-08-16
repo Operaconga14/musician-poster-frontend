0eae0551b65078f12a184e147b3082e60d666d81
Operaconga14 <operacongahub@gmail.com>
2024-08-16 09:03:21 +0100

    Some changes with seo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Meta, Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 import { DefaultLayoutComponent } from "./components/layouts/default-layout/default-layout.component";
 
@@ -11,4 +12,19 @@ import { DefaultLayoutComponent } from "./components/layouts/default-layout/defa
 })
 export class AppComponent {
   title = 'Mugivies';
+
+  private meta = inject(Meta)
+  private defualtTitle = inject(Title)
+
+
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.defualtTitle.setTitle(this.title)
+    this.meta.addTags([
+      { name: 'description', content: 'Post Gadgets, Products, Services, Gigs, Vancanies, New Musics for other musicians to see and See Post Gadgets, Products, Services, Gigs, Vancanies, New Musics of your interest from other musicians as well' },
+      { name: 'keywords', content: 'Music, Gadgets, Bass Guuitar, Piano, Posts, Gigs, Audios, Production, Music Production, Class, Vacancy, Vacancies, Events, Churches, Needed, Money' }
+    ])
+  }
 }
